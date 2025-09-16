410518befe25a6e143db651b30dec4ec9e0795f1
LAPTOP-RBNQ6FGR\minhp <minhphunguyenhoang0603@gmail.com>
2025-09-16 18:51:28 +0700

    Init for EX7

--- STM32/Core/Inc/software_timer.h
+++ STM32/Core/Inc/software_timer.h
@@ -8,9 +8,13 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+extern int timer0_flag;
 extern int timer1_flag;
+extern int timer2_flag;
 
-void setTimer1(int duration);
+void setTimer0 (int duration);
+void setTimer1 (int duration);
+void setTimer2 (int duration);
 void timerRun();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-//#include "software_timer.h"
+#include "software_timer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,22 +56,6 @@ void updateClockBuffer(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int timer0_counter = 0;
-int timer0_flag = 0;
-int TIMER_CYCLE = 10;
-void setTimer0(int duration) {
-    timer0_counter = duration / TIMER_CYCLE;
-    timer0_flag = 0;
-}
-void timer_run(void) {
-    if (timer0_counter > 0) {
-        timer0_counter--;
-        if (timer0_counter == 0) {
-            timer0_flag = 1;
-        }
-    }
-}
-
 const int MAX_LED = 4;
 int index_led = 0;
 int led_buffer[4] = {1, 2, 3, 4};
@@ -114,11 +98,11 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  hour = 15;
-  minute = 8;
-  second = 50;
+  hour = 15; minute = 8; second = 50;
 
-  setTimer0(100);
+  setTimer0(100); // LED Blinky
+  setTimer1(100); // DOT
+  setTimer2(100); // UPDATE TIME
   while (1)
   {
 	  if (timer0_flag == 1) {
@@ -126,20 +110,27 @@ int main(void)
 		  setTimer0(100);
 	  }
 
-	  second++;
-	  if (second >= 60) {
-		  second = 0;
-	      minute++;
+	  if (timer1_flag == 1) {
+		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+		  setTimer1(100);
 	  }
-	  if (minute >= 60) {
-	      minute = 0;
-	      hour++;
+
+	  if (timer2_flag == 1) {
+		  second++;
+		  if (second >= 60) {
+			  second = 0;
+			  minute++;
+		  }
+		  if (minute >= 60) {
+			  minute = 0;
+			  hour++;
+		  }
+		  if (hour >= 24) {
+			  hour = 0;
+		  }
+		  updateClockBuffer();
+		  setTimer2(100);
 	  }
-	  if (hour >= 24) {
-	      hour = 0;
-	  }
-	  updateClockBuffer();
-	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -407,13 +398,11 @@ void updateClockBuffer() {
     led_buffer[3] = minute % 10;
 }
 
-int led_counter = 100;
 int seg_counter = 25;
-int dot_counter = 100;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	timer_run();
+	timerRun();
 
 	seg_counter--;
 	if(seg_counter <= 0) {
@@ -423,12 +412,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		if(index_led >= MAX_LED) {
 			index_led = 0;
 		}
-	}
-
-	dot_counter--;
-	if(dot_counter <= 0) {
-		dot_counter = 100;
-		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	}
 }
 /* USER CODE END 4 */
