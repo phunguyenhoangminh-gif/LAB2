2d9a3c06507f0b08f34889e0bd6d700fa20c4f00
LAPTOP-RBNQ6FGR\minhp <minhphunguyenhoang0603@gmail.com>
2025-09-29 12:12:08 +0700

    Update EX9

--- STM32/Core/Src/software_timer.c
+++ STM32/Core/Src/software_timer.c
@@ -7,79 +7,30 @@
 
 #include "software_timer.h"
 
-int timer0_counter = 0;
-int timer0_flag = 0;
+#define MAX_TIMER 10
 
-int timer1_counter = 0;
-int timer1_flag = 0;
+int timer_counter[MAX_TIMER];
+int timer_flag[MAX_TIMER];
 
-int timer2_counter = 0;
-int timer2_flag = 0;
-
-int timer3_counter = 0;
-int timer3_flag = 0;
-
-int timer4_counter = 0;
-int timer4_flag = 0;
-
-void setTimer0 (int duration) {
-	timer0_counter = duration;
-	timer0_flag = 0;
+void setTimer (int index, int duration) {
+	timer_counter[index] = duration;
+	timer_flag[index] = 0;
 }
 
-void setTimer1 (int duration) {
-	timer1_counter = duration;
-	timer1_flag = 0;
-}
-
-void setTimer2 (int duration) {
-	timer2_counter = duration;
-	timer2_flag = 0;
-}
-
-void setTimer3 (int duration) {
-	timer3_counter = duration;
-	timer3_flag = 0;
-}
-
-void setTimer4 (int duration) {
-	timer4_counter = duration;
-	timer4_flag = 0;
+int isTimerExpired (int index) {
+	if (timer_flag[index] == 1) {
+		return 1;
+	}
+	return 0;
 }
 
 void timerRun() {
-	if (timer0_counter > 0) {
-		timer0_counter--;
-		if (timer0_counter <= 0) {
-			timer0_flag = 1;
-		}
-	}
-
-	if (timer1_counter > 0) {
-		timer1_counter--;
-		if (timer1_counter <= 0) {
-			timer1_flag = 1;
-		}
-	}
-
-	if (timer2_counter > 0) {
-		timer2_counter--;
-		if (timer2_counter <= 0) {
-			timer2_flag = 1;
-		}
-	}
-
-	if (timer3_counter > 0) {
-		timer3_counter--;
-		if (timer3_counter <= 0) {
-			timer3_flag = 1;
-		}
-	}
-
-	if (timer4_counter > 0) {
-		timer4_counter--;
-		if (timer4_counter <= 0) {
-			timer4_flag = 1;
+	for (int i = 0; i < MAX_TIMER; i++) {
+		if (timer_counter[i] > 0) {
+			timer_counter[i]--;
+			if (timer_counter[i] <= 1) {
+				timer_flag[i] = 1;
+			}
 		}
 	}
 }
